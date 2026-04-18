47f1b8c7a524dfc73952936b72618cb4e90b201a
tsiongtemesgen03-cpu <tsiongtemesgen03@gmail.com>
2026-04-17 20:23:58 -0400

    Add files via upload

---
+++ app_.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>Price-Wise ✅</h1>
+    <p>Deployed successfully on Railway.</p>
+    """
+
+@app.route("/api/test")
+def test():
+    return jsonify({"status": "ok"})
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
